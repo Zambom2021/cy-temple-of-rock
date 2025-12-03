589c497c0770498087e4dbfb62b44f3edcfc9b34
zambom2021 <rzambom@hotmail.com>
2025-12-03 10:49:24 -0300

    ajuste para salvar screenshot e selecionar o navegador

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
   projectId: 'wizhg6',
   e2e: {
     baseUrl: "http://localhost:9090/",
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    screenshotOnRunFailure: false,
+    video: true,
     setupNodeEvents(on, config) {
       // ativa o Allure
       allureWriter(on, config);
